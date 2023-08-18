71918676ee5e87a0b1f3c6b8dcff37fb0f4636dc
Nathan-reitan <nathanreitan@gmail.com>
2023-08-18 17:53:59 -0600

    filter_list submitted and passed

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const arrayDiff = (a, b) =>{
      const result = a.filter((el) => !b.includes(el));
      return result;
 }
+
+const listFiltering = (list) => {
+     return list.filter((el)=> typeof el ==="number") 
+}
